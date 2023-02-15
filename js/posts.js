020becc77700ffc815dc64007b38154c3213ee17
Mikołaj Gawron <gawrmiko@gmail.com>
2023-02-15 19:45:33 +0100

    poproawki w czytleności

---
+++ js/posts.js
@@ -0,0 +1,4 @@
+const postList = document.querySelector('#post-list');
+const filterUserId = document.querySelector('#filter-user-id');
+const filterTitle = document.querySelector('#filter-title');
+const filterBody = document.querySelector('#filter-body');
