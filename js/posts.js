3938da91f9bb6346194ffb6476a3417c54062d79
Mikołaj Gawron <gawrmiko@gmail.com>
2023-02-15 23:48:18 +0100

    dodanie wyświetlanie postów na stronie - axios

--- js/posts.js
+++ js/posts.js
@@ -1,6 +1,37 @@
 import axios from 'axios'
 
 const getPostsData = async () => {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/posts/")
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts/')
     return res.data
 }
+
+const uploadePosts = async () => {
+    const posts = await getPostsData()
+    const postsContainer = document.getElementById('posts-bottom')
+    posts.forEach( (post) => {
+        postsContainer.innerHTML += `
+            <table>
+                <tr>
+                    <td>
+                        <b>User Id:</b>
+                        ${post.userId}
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Tytuł:</b>
+                        ${post.title}
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Treść:</b>
+                        ${post.body}
+                    </td>
+                </tr>
+            </table>
+        `
+    })
+}
+
+uploadePosts()
