40a91b85f7b4c43581dd86dad19393fe92570567
Mikołaj Gawron <gawrmiko@gmail.com>
2023-02-16 10:55:36 +0100

    dodaje komentarze ale nie przez przycisk

--- js/posts.js
+++ js/posts.js
@@ -7,31 +7,38 @@ const getPostsData = async () => {
 
 const uploadePosts = async () => {
     const posts = await getPostsData()
-    const postsContainer = document.getElementById('posts-bottom')
+    const postsContainer = document.getElementById('posts')
     posts.forEach( (post) => {
-        postsContainer.innerHTML += `
-            <table>
-                <tr>
-                    <td>
-                        <b>User Id:</b>
-                        ${post.userId}
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <b>Tytuł:</b>
-                        ${post.title}
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <b>Treść:</b>
-                        ${post.body}
-                    </td>
-                </tr>
-            </table>
+        const postContainer = document.createElement('div')
+        postContainer.classList.add('post')
+        postContainer.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            <button class="btn${post.id}">Comments</button>
+
+            <div class="comments" id="comments-${post.id}"></div>
         `
+        postsContainer.appendChild(postContainer)
     })
 }
 
 uploadePosts()
+
+const getCoomentsData = async (postId) => {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+    return res.data
+}
+
+const uploadeComments = async (postId) => {
+    const comments = await getCoomentsData(postId)
+    const commentsContainer = document.getElementById(`comments-${postId}`)
+    comments.forEach( (comment) => {
+        const commentContainer = document.createElement('div')
+        commentContainer.classList.add('comment')
+        commentContainer.innerHTML = `
+            <h3>${comment.name}</h3>
+            <p>${comment.body}</p>
+        `
+        commentsContainer.appendChild(commentContainer)
+    })
+}
