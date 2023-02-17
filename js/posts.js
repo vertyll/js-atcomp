3c7f303c45c436348f95b830f4b25737223f2b16
Mikołaj Gawron <gawrmiko@gmail.com>
2023-02-17 15:55:14 +0100

    wdrazanie filtra

--- js/posts.js
+++ js/posts.js
@@ -9,14 +9,17 @@ const postEngine = async (postId) => {
         const postContainer = document.createElement('div')
         postContainer.classList.add('post')
 
-        const postTitle = document.createElement('h2')
-        postTitle.innerHTML = post.title
+        const postTitle = document.createElement('h3')
+        postTitle.innerHTML = `Tytuł: ${post.title}`
 
         const postBody = document.createElement('p')
         postBody.innerHTML = post.body
 
+        const postAuthor = document.createElement('p')
+        postAuthor.innerHTML = `Autor: ${post.userId}`
+
         const buttonShowHide = document.createElement('button')
-        buttonShowHide.innerHTML = 'Comments'
+        buttonShowHide.innerHTML = 'Komentarze'
         buttonShowHide.classList.add('btn-show-hide')
 
         const postComments = document.createElement('div')
@@ -26,6 +29,7 @@ const postEngine = async (postId) => {
         postsContainer.appendChild(postContainer)
         postContainer.appendChild(postTitle)
         postContainer.appendChild(postBody)
+        postContainer.appendChild(postAuthor)
         postContainer.appendChild(buttonShowHide)
         postContainer.appendChild(postComments)
 
@@ -60,6 +64,69 @@ const postEngine = async (postId) => {
             }
         }) 
     })
+}
+
+const saveFilterSettings = () => {
+    let path = window.location.pathname.substring(1)
+    for(inputName in inputsFromPosts){
+        if(inputsFromPosts[inputName].type != 'button'){
+            localStorage.setItem(`${path}::${inputName}`, form[inputName].value);
+        }
+    }
+}
+
+window.addEventListener('beforeunload', () => {
+    saveFilterSettings();
+})
+
+let inputs = {
+    author: {
+        label: 'Autor:', 
+        defaultValue: '', 
+        filterKey: 'userId', 
+        type: 'input', 
+        inputType: 'number'
+    },
+    title: {
+        label: 'Tytuł:', 
+        defaultValue: '', 
+        filterKey: 'title', 
+        type: 'input', 
+        inputType: 'text'
+    },
+    content: {
+        label: 'Treść:', 
+        defaultValue: '', 
+        filterKey: 'body', 
+        type: 'input', 
+        inputType: 'text'
+    },
+    sortBy: {
+        label: 'Sortowanie po:', 
+        defaultValue: 'userId', 
+        type: 'select', 
+        selectOptions: ['userId', 'title', 'body'], 
+        textForOptions: ['Autor', 'Tytuł', 'Treść']
+    },
+    filter: {
+        type: 'button', 
+        id: 'filter', 
+        content: 'Filtruj', 
+        btnFunction: async () =>{
+        let filteredPosts = await filter('posts');
+        postBuilder(filteredPosts);
+        saveFilterSettings();
+        }
+    },
+    clean: {
+        type: 'button', 
+        id: 'clean', 
+        content: 'Wyczyść', 
+        btnFunction: async () =>{
+        clean();
+        await postsData();
+        }
+    },
 }
 
 postEngine()
