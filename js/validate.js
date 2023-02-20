3ef7982c937b676574a6fd2805d415b73163f848
Mikołaj Gawron <gawrmiko@gmail.com>
2023-02-20 08:47:33 +0100

    kolorki przetrzymywane w 2 zmiennych

--- js/validate.js
+++ js/validate.js
@@ -37,29 +37,33 @@ function addInfo() {
     div.replaceChild(newItem, oldItem)
 }
 
+const styleBorder = (element, color) => element.style.border = `0.1rem solid ${color}`
+const styleBorderRed = (element) => styleBorder(element, 'red')
+const styleBroderGreen = (element) => styleBorder(element, 'green')
+
 function checkName() {
-    if (regex.name.test(formValues.name.value)) formValues.name.style.border = '0.1rem solid green'
-    else formValues.name.style.border = '0.1rem solid red'
+    if (regex.name.test(formValues.name.value)) styleBroderGreen(formValues.name)
+    else styleBorderRed(formValues.name)
 }
 
 function checkSurname() {
-    if (regex.surname.test(formValues.surname.value)) formValues.surname.style.border = '0.1rem solid green'
-    else formValues.surname.style.border = '0.1rem solid red'
+    if (regex.surname.test(formValues.surname.value)) styleBroderGreen(formValues.surname)
+    else styleBorderRed(formValues.surname)
 }
 
 function checkEmail() {
-    if (regex.email.test(formValues.email.value)) formValues.email.style.border = '0.1rem solid green'
-    else formValues.email.style.border = '0.1rem solid red'
+    if (regex.email.test(formValues.email.value)) styleBroderGreen(formValues.email)
+    else styleBorderRed(formValues.email)
 }
 
 function checkDescription() {
-    if (formValues.description.value !== '') formValues.description.style.border = '0.1rem solid green'
-    else formValues.description.style.border = '0.1rem solid red'
+    if (formValues.description.value !== '') styleBroderGreen(formValues.description)
+    else styleBorderRed(formValues.description)
 }
 
 function checkPesel() {
-    if (regex.pesel.test(formValues.pesel.value)) formValues.pesel.style.border = '0.1rem solid green'
-    else formValues.pesel.style.border = '0.1rem solid red'
+    if (regex.pesel.test(formValues.pesel.value)) styleBroderGreen(formValues.pesel)
+    else styleBorderRed(formValues.pesel)
 }
 
 export {
